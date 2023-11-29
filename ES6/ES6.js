607ae9114971fc0b093c3cf55ef1a2b42fea0177
adakasnr <adakasnr@gmail.com>
2023-11-29 16:09:29 +0530

    ES6

---
+++ ES6/ES6.js
@@ -0,0 +1,45 @@
+/*
+=>key words
+
+=>object- function in object is refred to a method
+definig method in object
+ 
+ genral way to create a function in object
+  const person = {
+    name: "Siva",
+    income: function (params) {
+        
+    },
+    salary () {}                 // new way to create a function
+  }
+
+  two ways to acees to key words
+  person.name();              // dot notation
+  person.name = "";
+  person["name"]= "Sriman";   // bracket notation
+
+=>THIS keyWord 
+methods- bind 
+
+=>arrow functions
+ 
+named function 
+const square = function (params) {
+    return number * number ;
+}
+
+const squre (params) => number * number;   //if single params we can exclude paranthisis
+
+we make the function in shortest form 
+default return key
+stand alone function  
+
+=> Array.map
+used to render the list of items 
+map method returns new array without modifying original one 
+
+
+=>object destructuring
+when we need to acess the keys in object, every time we need to use object name
+to avoid that we will be using destructring 
+*/
