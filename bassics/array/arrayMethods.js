0ab909abfba7ca7beb92f5da81743b6516686ad4
adakasnr <adakasnr@gmail.com>
2024-03-31 12:30:58 +0530

    array methods

--- bassics/array/arrayMethods.js
+++ bassics/array/arrayMethods.js
@@ -7,7 +7,7 @@ const array = ["element1", "element2", "element3", "element4"]
 console.log("at-", array.at(2));
 
 // concat()- it merges two or more arrays and returns new array
-const array1 = ["element1", "element2"];
+const array1 = ["element1:yes", "element2:no"];
 const array2 = ["element3", "element4"];
 
 const newArray = array1.concat(array2);
@@ -65,19 +65,52 @@ for (const key of iterator) {
 }
 
 
-// pop()-it removes the last element of array 
+// pop()-it removes the last element of array and returns that removed element 
+console.log("pop",array.pop());
+
+// push()-it adds new element to the end of array and retuns length of the array
+console.log("push",array.push("element5"));
+
+// map()-it makes changes to the array as per the given condition and return new mapped array
+const map1 = arrayWithEven.map((x)=>x/2)
+console.log(map1);
+
+// reduse()-
+// reverse-it reverse the array, the first element becomes last and the last element becomes frist element 
+const reverse = array1.reverse();
+console.log("reverse",reverse);
+
+// server()-
 
 
-// push
-// nap
-// refuse
-// server
-// reverse
+// shift()-it removes the first element of the array and returns the removed element, if the array is empty it retuns undefined
+console.log("shift",array.shift());
 
-// shift
-// slice
-// some
-// splice
-// unshift
-// values
-// sort
+// slice()- it removes the perticular elements based on the index given, returns the sliced elements
+console.log("slice",array.slice(1,3));
+
+// some()- it tests whether at least one element passes the test based on the function given
+const even = (element) => element % 2 === !0;
+console.log("some",arrayWithEven.some(even));
+
+// splice()- it helps us to remove and add element at a time, returns removed element
+array1.splice(1,1,'element1.5')
+// 1st param defines from where the removing need to be start
+// 2nd param defines how many need to be deleted
+// 3rd param defines the element to be placed in the place of delete
+console.log("splice",array1);
+
+// unshift()- it adds new element at the starting of an array
+console.log("unshift",array.unshift("element 0.5"));
+console.log(array);
+
+// values(?)-it retuns the values of the array
+const iterator2 = array1.values();
+
+for (const value of iterator2) {
+    console.log("value",value);
+}
+
+// sort()- it sorts the array in assending or decending order
+arrayWithEven.sort();
+console.log(arrayWithEven);
