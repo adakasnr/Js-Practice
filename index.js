c832c7e58fe8112184f7cbb829b8cb8536b8b02d
adakasnr <adakasnr@gmail.com>
2024-11-08 22:32:50 +0530

    js basics-dataTypes,variable,mathsOperations

---
+++ index.js
@@ -0,0 +1,44 @@
+const personDetails = (...paramters) => {
+    console.log(paramters);
+}
+
+personDetails(1,2,4,5,{name : "sriman"});
+
+const sriman = [1,2]
+
+const siva = [...sriman, 3]
+
+console.log(siva);
+
+var myName = "Siva";
+console.log(myName)
+
+myName = "Sivanagaraju"
+var name = myName
+
+    console.log(myName)
+
+let a = 5;
+console.log(a);
+
+let b =1+a;
+console.log(b);
+
+let hisName = "siva";
+let sureName = "adaka";
+
+let fullName = sureName+hisName+"nagaraju";
+console.log(fullName);
+
+fullName = sureName+" "+hisName+"nagaraju";
+console.log(fullName);
+
+var increment = 0;
+increment = increment+1;
+console.log(increment);
+increment++ //shortest code (for decrement we use -- after the variable)
+console.log(increment);
+
+increment--;
+console.log(increment);
+
